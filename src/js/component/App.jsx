e930c1593f6b11782df26b0724ef66796bb3ef0e
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-07-05 23:04:43 +0000

    Followed orginal css goals added items tracker

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -11,41 +11,46 @@ export const App = () => {
             let taskObject = {label: inputValue, done: false}
             setPendingTaskList((prevPendingTaskList) => [...prevPendingTaskList, taskObject]);
             setInputValue('');
+            setItemsLeft(pendingTaskList.length + 1);
         }
     };
 
     const removeToDoItem = (task) => {
-        setPendingTaskList((pendingTaskList) => {
+        setPendingTaskList((pendingTaskList) => {    
             return pendingTaskList.filter((toDo) => toDo.label !== task)
+            
         })
     }
 
     return (
         <div className="container justify-content-center">   
             <div className="d-flex">   
-                <h1 className="h-75 mx-auto mt-5 title">To Do List!</h1>
+                <p className="h-75 mx-auto mt-5 title">Todos</p>
             </div> 
-            <div className="d-flex mx-auto">
-                <input type="text" onChange={e => setInputValue(e.target.value)} 
-                    onKeyDown={e => {if (e.key === "Enter")addToPendingTask();}} value={inputValue} className="mx-auto"/>
-            </div> 
-            <div className="mt-5">
-                <div id="pendingList" className="d-flex mx-auto">
-                    <div className="d-flex col-12">
-                        <h1 className="mx-auto">Pending</h1>
-                    </div>    
-                </div>
-                <div id="visualPendingTasks" className="mx-auto col-12">
-                    {pendingTaskList.map((item, index) => (
-                    <div className="d-flex justify-content-between taskContainer"  key={index}>
-                        <h2>{item.label}</h2>
-                        <div className="ms-auto">
-                            <h1 className="closer" onClick={() => removeToDoItem(item.label)}>X</h1>
+            <div className="bg-white">
+                <div className="d-flex mx-auto inputDiv">
+                    <input type="text" onChange={e => setInputValue(e.target.value)} 
+                        onKeyDown={e => {if (e.key === "Enter")addToPendingTask();}} value={inputValue} className="ms-5 mt-5 mb-3"
+                        placeholder="What needs to be done?"/>
+                </div> 
+                <div className="mt-5 listHolder">
+                    <div id="visualPendingTasks" className="col-12">
+                        {pendingTaskList.map((item, index) => (
+                        <div className="d-flex justify-content-between taskContainer"  key={index}>
+                            <h2 className="ms-5 mt-2" >{item.label}</h2>
+                            <div className="ms-auto">
+                                <h1 className="closer me-3" onClick={() => removeToDoItem(item.label)}>X</h1>
+                            </div>
                         </div>
+                        ))}
                     </div>
-                    ))}
+                    <div>
+                        <p className="numberLeft">{pendingTaskList.length} item left</p>
+                    </div>        
                 </div>    
             </div>
+            <div className="firstEdge bg-white mx-auto"></div>
+            <div className="secondEdge bg-white mx-auto"></div>
         </div>
     );
 };
